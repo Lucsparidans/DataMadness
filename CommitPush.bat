c7af576bfef8b8059950fbbae8730f34a5f6a4ad
Lucsparidans <lucsnadiraps@gmail.com>
2021-03-22 11:24:32 +0100

    Work

--- CommitPush.bat
+++ CommitPush.bat
@@ -2,12 +2,12 @@ git add *
 git status
 if [%1]==[] goto dflt
 git commit -m %1
-git push origin master
+git push origin main
 pause
 goto :eof
 
 :dflt
 git commit -m "No commit msg"
-git push origin master
+git push origin main
 pause
 exit /B 1
